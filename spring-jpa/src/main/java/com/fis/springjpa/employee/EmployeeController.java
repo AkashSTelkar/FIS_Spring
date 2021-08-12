09a12b87fdfded8af4cf824acf060893798e9bfd
Akash Telkar <akashstelkar999@gmail.com>
2021-08-12 13:55:45 +0530

    performed CRUD operation on Employee data

---
+++ spring-jpa/src/main/java/com/fis/springjpa/employee/EmployeeController.java
@@ -0,0 +1,56 @@
+package com.fis.springjpa.employee;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/employee")
+public class EmployeeController {
+
+	@Autowired
+	private EmployeeRepository employeeRepository;
+
+	@PostMapping
+	public String addNewEmployee(@RequestBody Employee employee) {
+		employeeRepository.save(employee);
+		return "Saved";
+	}
+
+	// displaying an employee
+	@GetMapping
+	public @ResponseBody Iterable<Employee> getAllEmployee() {
+		return employeeRepository.findAll();
+	}
+
+	@PutMapping
+	public String updateEmployee(@RequestBody Employee employee) {
+		Optional<Employee> e = employeeRepository.findById(employee.getId());
+		if (e.isPresent()) {
+			employeeRepository.save(employee);
+			return "Updated";
+		} else {
+			return "Employee Not Found!!";
+		}
+	}
+
+	@DeleteMapping
+	public String deleteEmployee(@RequestParam int id) {
+		Optional<Employee> e = employeeRepository.findById(id);
+		if (e.isPresent()) {
+			employeeRepository.delete(e.get());
+			return "Deleted";
+		} else {
+			return "Employee Not Found!!";
+		}
+	}
+}
--- spring-jpa/src/main/java/com/fis/springjpa/product/ProductController.java
+++ spring-jpa/src/main/java/com/fis/springjpa/product/ProductController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+//@Controller
 @RestController
 @RequestMapping(path = "/product")
 public class ProductController {
