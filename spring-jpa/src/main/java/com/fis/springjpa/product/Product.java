4c501c4c37689c630ec4104fcc29fb924d51931e
Akash Telkar <akashstelkar999@gmail.com>
2021-08-12 12:33:28 +0530

    added crud operation for product

--- spring-jpa/src/main/java/com/fis/springjpa/product/Product.java
+++ spring-jpa/src/main/java/com/fis/springjpa/product/Product.java
@@ -12,21 +12,25 @@ public class Product {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
 	private String name;
-
-	public Integer getId() {
+	private double price;
+	public int getId() {
 		return id;
 	}
-
-	public void setId(Integer id) {
+	public void setId(int id) {
 		this.id = id;
 	}
-
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
 
+	
 }
